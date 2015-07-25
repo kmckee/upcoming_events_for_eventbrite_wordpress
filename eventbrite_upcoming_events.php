c5e119777f5ef06451983890de636b15fa36e64e
Kyle McKee <mckee.kyle@gmail.com>
2015-07-25 09:15:52 -0400

    Changed name of plugin for copyright reasons.  Max_count bug fixed.

--- eventbrite_upcoming_events.php
+++ eventbrite_upcoming_events.php
@@ -3,7 +3,7 @@
 Plugin Name: Eventbrite Upcoming Events 
 Plugin URI:  http://www.github.com/kmckee/eventbrite_upcoming_events
 Description: This plugin adds a widget that displays a list of upcoming events from eventbrite.
-Version:     0.1 
+Version:     0.2 
 Author:      Kyle McKee 
 Author URI:  http://aptobits.com
 License:     GPL2
@@ -12,25 +12,25 @@ License URI: https://www.gnu.org/licenses/gpl-2.0.html
 
 defined( 'ABSPATH' ) or die( 'Ah ah ah, you didn\'t say the magic word' );
 
-$app_key_id = 'eue_app_key';
-$organizer_id = 'eue_organizer_id';
+$app_key_id = 'uee_app_key';
+$organizer_id = 'uee_organizer_id';
 
 add_option($app_key_id);
 add_option($organizer_id);
 
-add_action( 'admin_menu', 'eventbrite_upcoming_plugin_menu' );
-function eventbrite_upcoming_plugin_menu() {
-    add_options_page( 'EventBrite Upcoming Events Options', 'Eventbrite Upcoming Events', 'manage_options', 'eventbrite_upcoming_events', 'eventbrite_upcoming_options' );
+add_action( 'admin_menu', 'upcoming_events_plugin_menu' );
+function upcoming_events_plugin_menu() {
+    add_options_page( 'Upcoming Events Options', 'Upcoming Events for EventBrite', 'manage_options', 'upcoming_events_eventbrite', 'upcoming_events_options' );
 }
-function eventbrite_upcoming_options() {
+function upcoming_events_options() {
     if (!current_user_can('manage_options'))
     {
         wp_die( __('You do not have sufficient permissions to access this page.') );
     }
 
-    $hidden_field_name = 'eue_submit_hidden';
-    $app_key_id = 'eue_app_key';
-    $organizer_id = 'eue_organizer_id';
+    $hidden_field_name = 'uee_submit_hidden';
+    $app_key_id = 'uee_app_key';
+    $organizer_id = 'uee_organizer_id';
 
     $app_key_val = get_option($app_key_id);
     $organizer_val = get_option($organizer_id);
@@ -46,7 +46,7 @@ function eventbrite_upcoming_options() {
     }
 
     echo '<div class="wrap">';
-    echo "<h2>" . __( 'Eventbrite Upcoming Events Plugin Settings', 'menu-test' ) . "</h2>";
+    echo "<h2>" . __( 'Upcoming Events For EventBrite Settings', 'menu-test' ) . "</h2>";
 ?>
 
 <form name="form1" method="post" action="">
@@ -71,19 +71,28 @@ function eventbrite_upcoming_options() {
 
 }
 
-add_shortcode('eventbrite_upcoming_events', 'inject_eventbrite_events');
-function inject_eventbrite_events() {
-    $app_key = get_option('eue_app_key');
-    $organizer_id = get_option('eue_organizer_id');
+add_shortcode('upcoming_events_for_eventbrite', 'inject_upcoming_events');
+function inject_upcoming_events($atts) {
+    $a = shortcode_atts( array(
+        'max_count' => '3'
+    ), $atts );   
+    $max_count = intval($a['max_count']);
+    $app_key = get_option('uee_app_key');
+    $organizer_id = get_option('uee_organizer_id');
     $url = "https://www.eventbrite.com/json/organizer_list_events?app_key=".$app_key."&id=".$organizer_id;
     $json = file_get_contents($url);
     $event_data = json_decode($json);
-    echo '<div class="eue_events">';
+    echo '<div class="uee_events">';
+    $i = 0;
     foreach ($event_data->events as $event) {
-        echo '<div class="eue_event">';
-        echo '<a href="'.$event->event->url.'"class="eue_event_title">'.$event->event->title.'</a> ';
+        $i += 1;
+        if ($i >$max_count) {
+            break;
+        };
+        echo '<div class="uee_event">';
+        echo '<a href="'.$event->event->url.'"class="uee_event_title">'.$event->event->title.'</a> ';
         $parsed_date = date_create_from_format('Y-m-d H:i:s', $event->event->start_date);
-        echo '<span class="eue_event_date">'.$parsed_date->format('m-d-Y h:ia')."</span>";
+        echo '<span class="uee_event_date">'.$parsed_date->format('m-d-Y h:ia')."</span>";
         echo '</div>';
     }
     echo '</div>';
